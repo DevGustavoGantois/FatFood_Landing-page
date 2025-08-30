604ed44f7163f7f031d9bbca8c8da248d34c6f23
Gustavo Gantois <gugagantois@gmail.com>
2025-08-30 09:08:39 -0300

    feat:Create contact section.

---
+++ src/app/s-contact.tsx
@@ -0,0 +1,9 @@
+export function Contact() {
+    return (
+        <>
+        <section className="max-w-[1440px] mx-auto p-8 lg:p-0 mt-10 lg:mt-[100px]" id="contato">
+            
+        </section>
+        </>
+    )
+}
