6e5e5c002bb607acd243e1aa51028586c39d1123
Gustavo Gantois <gugagantois@gmail.com>
2025-08-30 00:26:02 -0300

    style:Adjusting styles in footer.

--- src/app/global/c-footer.tsx
+++ src/app/global/c-footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <footer className="w-full bg-gradient-to-b from-[#E53935] to-[#7F201D]/80 py-10 mt-20 lg:mt-[100px] rounded-t-3xl">
       {footerData.map((item, index) => (
-        <div key={index} className="max-w-[1440px] mx-auto px-6">
+        <div key={index} className="px-6">
           <div className="flex flex-col gap-6 items-center justify-center text-center">
             <Image
               src={item.logo}
@@ -15,7 +15,7 @@ export function Footer() {
               alt="Logo"
               priority
             />
-            <p className="text-white text-center max-w-[750px]">
+            <p className="text-white text-center max-w-[600px]">
               {item.description}
             </p>
             <div className="flex gap-4">
@@ -45,9 +45,11 @@ export function Footer() {
                 </Link>
               ))}
             </nav>
-            <div className="border-t border-gray-200/20 w-full mt-6 pt-4 flex flex-col lg:flex-row items-center justify-between gap-2">
-              <p className="text-white text-sm">{item.copyright}</p>
-              <p className="text-white text-sm">{item.footerCopy}</p>
+            <div className="border-t border-gray-200/20 w-full pt-4 flex flex-col lg:flex-row items-center justify-between gap-2">
+              <div className="max-w-[1440px] mx-auto flex items-center justify-between w-full">
+                <p className="text-white text-sm">{item.copyright}</p>
+                <p className="text-white text-sm">{item.footerCopy}</p>
+              </div>
             </div>
           </div>
         </div>
