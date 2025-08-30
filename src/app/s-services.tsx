3c0d2e1d1e3b959567b75e28401f947fc23e03a1
Gustavo Gantois <gugagantois@gmail.com>
2025-08-30 11:28:36 -0300

    feat:Create service section.

--- src/app/s-services.tsx
+++ src/app/s-services.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 
 export function Services() {
   return (
-    <section className="max-w-[1440px] mx-auto p-8 lg:p-0 mt-10 lg:mt-[100px]">
+    <section className="max-w-[1440px] mx-auto p-8 mt-10 lg:mt-[100px]">
       {servicesData.map((item, index) => (
         <main key={index} className="flex flex-col gap-[16px]">
-          {/* título e botão */}
-          <article className="flex flex-col items-center gap-4 justify-center lg:flex-row lg:justify-between">
+          <article className="flex flex-col items-center gap-4 justify-center lg:flex-row lg:justify-between mb-[20px] lg:mb-[44px]">
             <h1 className="text-4xl lg:text-6xl font-bold text-[#E53935]">
               {item.title}
             </h1>
@@ -15,9 +14,7 @@ export function Services() {
               {item.btnText}
             </button>
           </article>
-
-          {/* layout dos cards */}
-          <div className="flex flex-col lg:flex-row items-start justify-center gap-6">
+          <div className="flex flex-col lg:flex-row items-center lg:items-start justify-center gap-6">
             <div className="flex-1 w-full h-full">
               <div className="rounded-2xl overflow-hidden h-full flex flex-1 flex-col">
                 <Image
@@ -25,23 +22,21 @@ export function Services() {
                   width={500}
                   height={400}
                   alt={item.cards[0].title}
-                  className="w-full h-[350px] object-cover"
+                  className="w-full h-[700px] rounded-4xl object-cover"
                 />
                 <div className="p-6 text-left">
-                  <h2 className="font-bold text-2xl text-black mb-2">
+                  <h2 className="font-bold text-2xl text-black text-center lg:text-start mb-2">
                     {item.cards[0].title}
                   </h2>
-                  <p className="text-[#898989]">{item.cards[0].description}</p>
+                  <p className="text-[#898989] text-base lg:text-xl text-center lg:text-start">{item.cards[0].description}</p>
                 </div>
               </div>
             </div>
-
-            {/* cards menores à direita */}
             <div className="flex flex-col gap-6 flex-1 w-full">
               {item.cards.slice(1).map((card, idx) => (
                 <div
                   key={idx}
-                  className="rounded-2xl soverflow-hidden flex items-center gap-4 p-4"
+                  className="rounded-2xl soverflow-hidden flex flex-col justify-center lg:flex-row text-center lg:text-start items-center gap-4 p-4"
                 >
                   <Image
                     src={card.img}
@@ -51,10 +46,10 @@ export function Services() {
                     className="rounded-lg"
                   />
                   <div className="flex flex-col">
-                    <h3 className="font-bold text-xl text-black mb-1">
+                    <h3 className="font-bold text-2xl text-black mb-1">
                       {card.title}
                     </h3>
-                    <p className="text-[#898989] text-sm max-w-[300px]">{card.description}</p>
+                    <p className="text-[#898989] max-w-[500px] text-base lg:text-xl">{card.description}</p>
                   </div>
                 </div>
               ))}
